4eeed776b3dcebb2ccd51ff0bece9a79a7de91ee
mac <macdev2001@gmail.com>
2023-10-31 02:16:27 +0000

    code format

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import Navbar from './Navbar.jsx'
+import Navbar from "./Navbar.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Navbar />
     <App />
   </React.StrictMode>,
-)
+);
