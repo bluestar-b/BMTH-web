85bfb4ad23722798457b94b667d8124044f0c762
mac <macdev2001@gmail.com>
2023-10-31 19:18:56 +0000

    idk

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,14 +1,23 @@
 const About = () => {
+  const titleClass =
+    "font-md collapse-title rounded-lg border-2 text-xl text-gray-200 backdrop-blur-lg";
+  const contentClass =
+    "collapse-content mt-2 rounded-md border-2 border-gray-300 backdrop-blur-lg";
+  const aboutData = [
+    {
+      topic_title: "About",
+      content: "This is the about page content.",
+    },
+  ];
+
   return (
     <>
       <div className="collapse bg-transparent p-4 text-gray-200">
         <input type="checkbox" className="peer" />
-        <div className="font-md collapse-title rounded-lg border-2 text-xl text-gray-200 backdrop-blur-lg">
-          test
-        </div>
+        <div className={titleClass}>test</div>
 
-        <div className="collapse-content mt-2 rounded-md border border-gray-300 backdrop-blur-lg">
-          <p>We killed whole jews</p>
+        <div className={contentClass}>
+          <p>Test</p>
         </div>
       </div>
     </>
