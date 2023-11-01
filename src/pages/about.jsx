9cd240b18def459ee61e3b8d9c9e83095f585282
mac <macdev2001@gmail.com>
2023-11-01 00:56:52 +0000

    dumbest solution lmao

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,27 +1,43 @@
 const About = () => {
+  
   const titleClass =
     "font-md collapse-title rounded-lg border-2 text-xl text-gray-200 backdrop-blur-lg";
   const contentClass =
-    "collapse-content mt-2 rounded-md border-2 border-gray-300 backdrop-blur-lg";
+    "collapse-content mt-1 rounded-md border-2 border-gray-300 backdrop-blur-lg text-gray-200";
   const aboutData = [
     {
       topic_title: "About",
       content: "This is the about page content.",
     },
+    {
+      topic_title: "Mission",
+      content: "Our mission is to create great products.",
+    },
+    {
+      topic_title: "Vision",
+      content: "We aim to change the world with our innovations.",
+    },
   ];
 
   return (
     <>
-      <div className="collapse bg-transparent p-4 text-gray-200">
-        <input type="checkbox" className="peer" />
-        <div className={titleClass}>test</div>
-
-        <div className={contentClass}>
-          <p>Test</p>
-        </div>
-      </div>
+      {aboutData.map((data, index) => (
+        <details className="collapse bg-transparent lg:p-4" key={index}>
+          <summary className={titleClass}>
+            {data.topic_title}
+          </summary>
+          <div className={contentClass}>
+            <p>{data.content}</p>
+          </div>
+        </details>
+      ))}
     </>
   );
 };
 
 export default About;
+
+
+
+
+
